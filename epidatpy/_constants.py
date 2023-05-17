0c160aa173b17ba0d4d3efe181fc2665e84116c7
george haff <george.haff@gmail.com>
2023-05-17 15:06:22 -0400

    updated to new api hostname -- DO NOT MERGE UNTIL CERT IS IN PLACE

--- epidatpy/_constants.py
+++ epidatpy/_constants.py
@@ -6,4 +6,4 @@ __version__: Final = "0.5.0"
 
 HTTP_HEADERS: Final = {"User-Agent": f"epidatpy/{__version__}"}
 
-BASE_URL: Final = "https://api.covidcast.cmu.edu/epidata/"
+BASE_URL: Final = "https://api.delphi.cmu.edu/epidata/"
