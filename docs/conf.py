2a50be64f998fc359aa70ffc949181e011072c94
Dmitry Shemetov <1810426+dshemetov@users.noreply.github.com>
2026-05-15 22:58:37 -0700

    ci: remove bump2version

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from importlib.metadata import version
 
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("../epidatpy"))
@@ -23,7 +24,7 @@ copyright = "2024, Delphi Research Group"  # pylint: disable=redefined-builtin
 author = "Delphi Research Group"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = version("epidatpy")
 
 
 # -- General configuration ---------------------------------------------------
--- epidatpy/_constants.py
+++ epidatpy/_constants.py
@@ -1,6 +1,7 @@
+from importlib.metadata import version
 from typing import Final
 
-__version__: Final = "0.5.0"
+__version__: Final = version("epidatpy")
 HTTP_HEADERS: Final = {"User-Agent": f"epidatpy/{__version__}"}
 BASE_URL: Final = "https://api.delphi.cmu.edu/epidata/"
 CAST_BASE_URL: Final = "https://delphi.cmu.edu/epidata/v5/"
